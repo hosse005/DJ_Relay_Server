73620d17b58292c42f0761902f97fd09d5a949d8
Evan Hosseini <hossein2@uwm.edu>
2013-10-29 21:05:32 -0500

    Add first version of the API.  Added support to the relayTable for staging of randomly selected user.  Turned off notice error reporting.

--- DJ_Relay_Server/relayTable.php
+++ DJ_Relay_Server/relayTable.php
@@ -1,4 +1,8 @@
 <?php
+
+// Report all errors except E_NOTICE
+error_reporting(E_ALL ^ E_NOTICE);
+
 class relayTable {
 	
 	// TODO - Need to replace all SQL queries w/ prepared statements!!!
@@ -46,9 +50,35 @@ class relayTable {
 			$this->replyJSON['dbCon'] = true;
 	}
 	
+	function table_exist($table){
+		$sql = "SHOW TABLES LIKE '".$table."'";
+		$res = mysqli_query($this->con, $sql);
+		return ($res->num_rows > 0);
+	}
+	
+	function fetchRecord($userid, $tableName){
+		$sql = "SELECT * FROM `" . $tableName ."` WHERE userid = '$userid' ";
+		$res = mysqli_query($this->con, $sql);
+	
+		if (!$res) {
+			$this->replyJSON['masterPresent'] = false;
+			return;
+		} else
+			$this->replyJSON['masterPresent'] = true;
+		return mysqli_fetch_assoc($res);
+	}
+
+	
 	function createTable($master, $tableName) {
 		$this->master = $master;
 		$this->tableName = $tableName;
+
+		// First Check if table exists
+		if ($this->table_exist($tableName)) {
+			$this->replyJSON['sessionAlreadyExists'] = true;
+			return;
+		}
+		
 		$sql = "CREATE TABLE `" . $this->tableName . "`
 		(
 		PID INT NOT NULL AUTO_INCREMENT,
@@ -57,7 +87,8 @@ class relayTable {
 		user VARCHAR(50),
 		ip VARCHAR(50),
 		selected_tubeID VARCHAR(50),
-		selected_userID VARCHAR(50)
+		selected_userID VARCHAR(50),
+		nxtSelected_userID VARCHAR(50)
 		)";
 		
 		if (mysqli_query ( $this->con, $sql )) {
@@ -70,12 +101,13 @@ class relayTable {
 		}
 		
 		// Insert master to the table as PID = 1, and initialize w/ das golden Scheiße
-		$sql = "INSERT INTO `" . $this->tableName . "` (userid,user,selected_tubeID,selected_userID) VALUES
+		$sql = "INSERT INTO `" . $this->tableName . "` (userid,user,selected_userID,nxtSelected_userID,selected_tubeID) VALUES
 				(
 				'{$this->master['userid']}',
 				'{$this->master['user']}',
-				'5NV6Rdv1a3I',
-				'{$this->master['userid']}'
+				'{$this->master['userid']}',
+				'{$this->master['userid']}',
+				'5NV6Rdv1a3I'
 				) ";
 		
 		if (mysqli_query ( $this->con, $sql )) {
@@ -87,11 +119,19 @@ class relayTable {
 			return;
 		}
 	}
+
 	
 	function updateUser($user, $tableName) {
 		// TODO - need to decide if something w/ ip checking should be enforced
 		// Maybe this is separate check to ensure requesting user ip doesn't already exist in table
 		// For now, we just check user id..
+		
+		// First Check if table exists
+		if (!$this->table_exist($tableName)) {
+			$this->replyJSON['sessionNotFound'] = true;
+			return;
+		}
+		
 		$userCheck = "SELECT COUNT(*) as count FROM  `" . $tableName . "` WHERE 
 					  userid='{$user['userid']}'";
 					 // OR ip='{$user['ip']}'";
@@ -101,7 +141,14 @@ class relayTable {
 		$userCheckResult = $userCheckResult ['count'];
 		
 		if ($userCheckResult > 0) {
-			//echo "userCheckResult > 0" . "/n";
+			// Check for user joining session that he/she/it is already master of
+			// Ideally, this is enforced on client side and prevented
+			// Unfortunately, I have also written the client side code so..
+			if ($user['selected_tubeID'] == null){
+				$this->replyJSON['masterJoinError'] = true;
+				return;
+			}
+			
 			$sql = "UPDATE `" . $tableName . "` SET selected_tubeID = '{$user['selected_tubeID']}' 
 			        WHERE userid = '{$user['userid']}'";
 			
@@ -132,54 +179,96 @@ class relayTable {
 	}
 	
 	function updateFeed($master, $tableName) {
+		// TODO - still need to add some null checking to prevent blank vid being selected
+		
+		// First check if session still exists
+		if (!$this->table_exist($tableName)) {
+			$this->replyJSON['sessionNotFound'] = true;
+			return;
+		}
+		
 		// Read out currently selected user from Master entry
 		$sql = "SELECT * FROM `" . $tableName ."` WHERE PID = 1";
 		$res = mysqli_query($this->con, $sql);
 		
 		if (!$res) {
-			//echo "Error finding master for updateFeed: " . mysqli_error($this->con). "<br>";
 			$this->replyJSON['masterPresent'] = false;
 			return;
 		} else
 			$this->replyJSON['masterPresent'] = true;
 		
 		$res = mysqli_fetch_assoc($res);
-		$selected_userID = $res['selected_userID'];
 		
-		// Randomly select next user, disallow current user to be selected again
+		// Only allow master of the session to make this call
+		if ($res['userid'] != $master['userid']){
+			$this->replyJSON['unauthorizedRequest'] = true;
+			return;
+		}
+		
+		$selected_userID = $res['selected_userID'];
+		$nxtSelected_userID = $res['nxtSelected_userID'];
+		
+		// First, update selected user pipeline held in the all records
+		$sql = "UPDATE `" . $tableName . "` SET selected_userID = '$nxtSelected_userID'"; 
+//			    WHERE userid = '{$master['userid']}'";
+		$res = mysqli_query($this->con, $sql);
+		if (!$res){
+			$this->replyJSON['updateFeed'] = false;		
+			return;
+		}
+		// Now, grab this record and echo to master client
+		$sql = "SELECT * FROM `" . $tableName . "` WHERE userid = '$selected_userID'";
+		$res = mysqli_query($this->con, $sql);
+		if (!$res) {
+			$this->replyJSON['updateFeed'] = false;	
+			return;
+		}
+		$row1 = mysqli_fetch_assoc($res);
+		$this->replyJSON['selectedUser'] = $row1;
+		
+		// Next, randomly select next user and disallow current user to be selected again
 		// TODO - also don't allow user to be selected w/ no tube ID (e.g. check against NULL)
-		// TODO - only let master user execute this command!
-		$sql = "SELECT * FROM `" . $tableName . "` WHERE userid != '$selected_userID'
+		$sql = "SELECT * FROM `" . $tableName . "` WHERE userid != '$nxtSelected_userID'
 				ORDER BY RAND() LIMIT 1";
 		$res = mysqli_query ( $this->con, $sql );
 		
 		if (! mysqli_num_rows ( $res ) > 0) {
-			//echo "No users joined this table yet <br>";
 			$this->replyJSON['usersPresent'] = false;
-			return;
-		} else
+			$row1 = array('userid' => $master['userid']);
+//			$row1 = json_encode($row1); 
+//			echo $row1;
+		} else {
 			$this->replyJSON['usersPresent'] = true;
+			$row1 = mysqli_fetch_assoc ( $res );
+		}			
 		
-		// Update master table with newly selected userid
-		$row1 = mysqli_fetch_assoc ( $res );
-		$sql = "UPDATE `" . $tableName . "` SET selected_userID = '{$row1['userid']}' 
-			    WHERE userid = '{$master['userid']}'";
+		// Next, update all records in table with newly selected userid
+		// Update master record pipe w/ randomly selected nxtSelected_userID
+		$sql = "UPDATE `" . $tableName . "` SET nxtSelected_userID = '{$row1['userid']}'"; 
+//			    WHERE userid = '{$master['userid']}'";
 		
 		if (mysqli_query ( $this->con, $sql )) {
-			//echo "<br>" . '$tableName'. " UPDATE feed command success with '{$row1['userid']}'";
 			$this->replyJSON['updateFeed'] = true;
-			$this->replyJSON['selectedUser'] = $row1;
-			//echo "<br>". json_encode($this->replyJSON['selectedUser']) . "<br>";
 		} else {
-			//echo "Error updating selected user to '$tableName': " . mysqli_error ( $this->con );
 			$this->replyJSON['updateFeed'] = false;
 			return;
 		}
+		
+		// Finally, echo updated master record
+		$this->replyJSON['selectedUser'] = $this->fetchRecord($nxtSelected_userID, $tableName);	// This is actually the selected user now,
+																								// stole from variable used before updating
+																								// the table.. pretty shitty i know
 	}
 	
 	function fetchCurrentFeed($tableName){
 		// TODO - Maybe want to add user validation query to this function..
-		// Read currently selected tube ID
+		
+		// First check if session still exists
+		if (!$this->table_exist($tableName)) {
+			$this->replyJSON['sessionNotFound'] = true;
+			return;
+		}
+		// Read currently selected tube ID		
 		$sql = "SELECT * FROM `" . $tableName ."` WHERE PID = 1";
 		$res = mysqli_query($this->con, $sql);
 		
@@ -253,7 +342,7 @@ class relayTable {
 			$this->replyJSON['sessionKill'] = false;
 		}
 	}
-	
+		
 	function __destruct() {
 		//echo "Destroying  " . $this->tableName . " object " . "<br>";
 		$this->replyJSON = json_encode($this->replyJSON);
